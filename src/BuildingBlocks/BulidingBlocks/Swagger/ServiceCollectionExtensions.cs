dc8152d3e5534d0de26adb38ec86a6fcd3bf7725
mehdihadeli <mehdi.hadeli@outlook.com>
2021-12-07 12:19:19 +0330

    .

--- src/BuildingBlocks/BulidingBlocks/Swagger/ServiceCollectionExtensions.cs
+++ src/BuildingBlocks/BulidingBlocks/Swagger/ServiceCollectionExtensions.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using BuildingBlocks.Security.ApiKey;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.PlatformAbstractions;
@@ -21,7 +23,12 @@ namespace BuildingBlocks.Swagger
         {
             services.AddVersionedApiExplorer(options =>
             {
+                // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                // note: the specified format code will format the version as "'v'major[.minor][-status]"
                 options.GroupNameFormat = "'v'VVV";
+
+                // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                // can also be used to control the format of the API version in route templates
                 options.SubstituteApiVersionInUrl = true;
             });
 
@@ -45,16 +52,16 @@ namespace BuildingBlocks.Swagger
                         options.IncludeXmlComments(xmlFile);
                     }
 
-                    // options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                    // {
-                    //     Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
-                    //           Enter 'Bearer' [space] and then your token in the text input below.
-                    //           \r\n\r\nExample: 'Bearer 12345abcdef'",
-                    //     Name = "Authorization",
-                    //     In = ParameterLocation.Header,
-                    //     Type = SecuritySchemeType.ApiKey,
-                    //     Scheme = "Bearer"
-                    // });
+                    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                              Enter 'Bearer' [space] and then your token in the text input below.
+                              \r\n\r\nExample: 'Bearer 12345abcdef'",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer"
+                    });
 
                     options.AddSecurityDefinition(ApiKeyConstants.HeaderName, new OpenApiSecurityScheme
                     {
@@ -66,20 +73,20 @@ namespace BuildingBlocks.Swagger
 
                     options.AddSecurityRequirement(new OpenApiSecurityRequirement()
                     {
-                        // {
-                        //     new OpenApiSecurityScheme
-                        //     {
-                        //         Reference = new OpenApiReference
-                        //         {
-                        //             Type = ReferenceType.SecurityScheme,
-                        //             Id = "Bearer"
-                        //         },
-                        //         Scheme = "oauth2",
-                        //         Name = "Bearer",
-                        //         In = ParameterLocation.Header,
-                        //     },
-                        //     new List<string>()
-                        // },
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header,
+                            },
+                            new List<string>()
+                        },
                         {
                             new OpenApiSecurityScheme
                             {
@@ -92,6 +99,25 @@ namespace BuildingBlocks.Swagger
                             new string[] { }
                         }
                     });
+
+                    options.TagActionsBy(api =>
+                    {
+                        if (api.GroupName != null)
+                        {
+                            return new[] { api.GroupName };
+                        }
+
+                        if (api.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+                        {
+                            return new[] { controllerActionDescriptor.ControllerName };
+                        }
+
+                        throw new InvalidOperationException("Unable to determine tag for endpoint.");
+                    });
+
+                    options.DocInclusionPredicate((name, api) => true);
+
+                    options.EnableAnnotations();
                 });
 
             return services;
