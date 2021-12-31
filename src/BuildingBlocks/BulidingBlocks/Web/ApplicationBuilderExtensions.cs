ef2fd5aa02dc4fa5873b8053e97bcb4fa65c3e89
mehdihadeli <mehdi.hadeli@outlook.com>
2021-12-31 21:43:17 +0330

    .

--- src/BuildingBlocks/BulidingBlocks/Web/ApplicationBuilderExtensions.cs
+++ src/BuildingBlocks/BulidingBlocks/Web/ApplicationBuilderExtensions.cs
@@ -1,3 +1,7 @@
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -6,46 +10,73 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace BuildingBlocks.Web
 {
+    //https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks
+    //https://nikiforovall.github.io/dotnet/aspnetcore/coding-stories/2021/07/25/add-health-checks-to-aspnetcore.html
     public static class ApplicationBuilderExtensions
     {
         public static IApplicationBuilder UseCustomHealthCheck(this IApplicationBuilder app)
         {
             app.UseHealthChecks("/healthz", new HealthCheckOptions
-            {
-                Predicate = _ => true,
-                // ResponseWriter = async (context, report) =>
-                // {
-                //     context.Response.ContentType = "application/json";
-                //
-                //     var result = JsonConvert.SerializeObject(new
-                //     {
-                //         Status = report.Status.ToString(),
-                //         HealthChecks = report.Entries.Select(x => new
-                //         {
-                //             Components = x.Key,
-                //             Status = x.Value.Status.ToString(),
-                //             Description = x.Value.Description
-                //         }),
-                //         HealthCheckDuration = report.TotalDuration
-                //     });
-                //
-                //     await context.Response.WriteAsync(result);
-                // },
-                //https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks
-                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
-                ResultStatusCodes =
                 {
-                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
-                    [HealthStatus.Degraded] = StatusCodes.Status500InternalServerError,
-                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
-                },
-            }).UseHealthChecksUI(setup =>
-            {
-                setup.ApiPath = "/healthcheck";
-                setup.UIPath = "/healthcheck-ui";
-            });
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+                    ResultStatusCodes =
+                    {
+                        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                        [HealthStatus.Degraded] = StatusCodes.Status500InternalServerError,
+                        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
+                    },
+                })
+                .UseHealthChecks("/health", new HealthCheckOptions
+                {
+                    Predicate = (check) => !check.Tags.Contains("services"),
+                    AllowCachingResponses = false,
+                    ResponseWriter = WriteResponseAsync,
+                })
+                .UseHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    AllowCachingResponses = false,
+                    ResponseWriter = WriteResponseAsync,
+                })
+                .UseHealthChecksUI(setup =>
+                {
+                    setup.ApiPath = "/healthcheck";
+                    setup.UIPath = "/healthcheck-ui";
+                });
 
             return app;
+        }
+
+        private static Task WriteResponseAsync(HttpContext context, HealthReport result)
+        {
+            context.Response.ContentType = "application/json; charset=utf-8";
+
+            var options = new JsonWriterOptions
+            {
+                Indented = true
+            };
+
+            using var stream = new MemoryStream();
+            using (var writer = new Utf8JsonWriter(stream, options))
+            {
+                writer.WriteStartObject();
+                writer.WriteString("status", result.Status.ToString());
+                writer.WriteStartObject("results");
+                foreach (var entry in result.Entries)
+                {
+                    writer.WriteStartObject(entry.Key);
+                    writer.WriteString("status", entry.Value.Status.ToString());
+                    writer.WriteEndObject();
+                }
+
+                writer.WriteEndObject();
+                writer.WriteEndObject();
+            }
+
+            var json = Encoding.UTF8.GetString(stream.ToArray());
+
+            return context.Response.WriteAsync(json);
         }
     }
 }
